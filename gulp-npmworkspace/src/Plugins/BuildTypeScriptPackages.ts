c7b232da4e525b9e19466b17fe41c23ce4bca1cd
timjroberts <tim@timjroberts.com>
2016-08-19 11:03:00 +0100

    revert 5183269

--- gulp-npmworkspace/src/Plugins/BuildTypeScriptPackages.ts
+++ gulp-npmworkspace/src/Plugins/BuildTypeScriptPackages.ts
@@ -158,12 +158,6 @@ function buildTypeScriptPackage(packageDescriptor: PackageDescriptor, packagePat
 
             Logger.info(util.colors.bold(`Compiling workspace package '${util.colors.cyan(packageDescriptor.name)}'`));
 
-            if (typeScriptConfigurations.length === 1) {
-                pluginBinding.shellExecuteTsc(packagePath, []);
-
-                return resolve();
-            }
-
             let tscArgFilePromises = typeScriptConfigurations.map((typeScriptConfiguration, idx) => new Promise<string>((resolve, reject) => {
                 let argsFileName = `_${idx.toString()}_${TSC_ARGS_FILENAME}`;
 
